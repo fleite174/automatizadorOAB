1a41a768d09ab34a3d0ef2c7fcd4e3f9faec9a50
fleite174 <andretsilva2509@gmail.com>
2023-08-21 12:00:54 -0300

    selecionar numero oab e estado

--- app.py
+++ app.py
@@ -3,11 +3,18 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from time import sleep
+
+numero_oab = 133864
+
 # entrar no site da - https://pje-consultapublica.tjdft.jus.br/
 driver =  webdriver.Chrome()
 driver.get('https://pje-consultapublica.tjdft.jus.br/')
 sleep(30)
-# digitar número oab e selecionar estado
+# digitar número oab 
+campo_oab = driver.find_element(By.XPATH,"//input[@id='fPP:Decoration:numeroOAB']")
+campo_oab.send_keys(numero_oab)
+# selecionar estado
+driver.find_element(By.XPATH,"//select[@id='fPP:Decoration:estadoComboOAB']")
 # clicar em pesquisar
 # entrar em cada um dos processos
 # extrair o número do processo e data da distribuição
