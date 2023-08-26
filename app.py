3ac251587ff69ce8aa334abc3f4cd763482dd543
fleite174 <andretsilva2509@gmail.com>
2023-08-26 15:46:31 -0300

    Codigo finalizado e funcionando

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import openpyxl
 numero_oab = 133864
 
 # entrar no site da - https://pje-consulta-publica.tjmg.jus.br/
-driver =  webdriver.Chrome()
+driver = webdriver.Chrome()
 driver.get('https://pje-consulta-publica.tjmg.jus.br/')
 sleep(30)
 # digitar número oab 
@@ -30,7 +30,7 @@ for link_processo  in link_processos:
     janelas = driver.window_handles
     driver.switch_to.window(janelas[-1])
     driver.set_window_size(1920, 1080)
-# extrair o número do processo e data da distribuição
+    # extrair o número do processo e data da distribuição
     numero_processo = driver.find_elements(By.XPATH,"//div[@class='col-sm-12 ']")
     numero_processo = numero_processo[0]
     numero_processo = numero_processo.text
@@ -38,50 +38,50 @@ for link_processo  in link_processos:
     data_distribuicao = driver.find_elements(By.XPATH,"//div[@class='value col-sm-12 ']")
     data_distribuicao = data_distribuicao[1]
     data_distribuicao = data_distribuicao.text
-# extrair e guardar todas as ultimas movimentações
-movimentacoes = driver.find_elements(By.XPATH,"//div[@id='j_id132:processoEventoPanel_body']//tr[contains(@class,'rich-table-row ')]//td//div//div//span")
-lista_movimentacoes = []
-for movimentacao in movimentacoes:
-    lista_movimentacoes.append(movimentacao.text)
-# guardar tudo no excel, separados por processo
-workbook = openpyxl.load_workbook('dados.xlsx')
-try:
-    # código para inserir dados em página existente
-    # acessar página do processo
-    pagina_processo = workbook[numero_processo]
-    # criar nome das colunas
-    pagina_processo['A1'].value = "Número Processo"
-    pagina_processo['B1'].value = "Data Distribuição"
-    pagina_processo['C1'].value = "Movimentações"
-    # adicionar número do processo
-    pagina_processo['A2'].value = numero_processo
-    # adicionar data de distribuição
-    pagina_processo['B2'].value = data_distribuicao
-    # adicionar movimentações
-    for index, linha in enumerate(pagina_processo.iter_rows(min_row=2,max_row=len(lista_movimentacoes),min_col=3,max_col=3)):
-        for celula in linha:
-            celula.value = lista_movimentacoes[index]
-    workbook.save('dados.xlsx')
-    driver.close()
-    sleep(5)
-    driver.switch_to.window(driver.window_handles[0])
-except Exception as error:
-    # código para criar uma pagina do zero e inserir as informações
-    workbook.create_sheet(numero_processo)
-    # acessar página do processo
-    pagina_processo = workbook[numero_processo]
-    # criar nome das colunas
-    pagina_processo['A1'].value = "Número Processo"
-    pagina_processo['B1'].value = "Data Distribuição"
-    pagina_processo['C1'].value = "Movimentações"
-    # adicionar número do processo
-    pagina_processo['A2'].value = numero_processo
-    # adicionar data de distribuição
-    pagina_processo['B2'].value = data_distribuicao
-    # adicionar movimentações
-    for index, linha in enumerate(pagina_processo.iter_rows(min_row=2,max_row=len(lista_movimentacoes),min_col=3,max_col=3)):
-        for celula in linha:
-            celula.value = lista_movimentacoes[index]
+    # extrair e guardar todas as ultimas movimentações
+    movimentacoes = driver.find_elements(By.XPATH,"//div[@id='j_id132:processoEventoPanel_body']//tr[contains(@class,'rich-table-row ')]//td//div//div//span")
+    lista_movimentacoes = []
+    for movimentacao in movimentacoes:
+        lista_movimentacoes.append(movimentacao.text)
+    # guardar tudo no excel, separados por processo
+    workbook = openpyxl.load_workbook('dados.xlsx')
+    try:
+        # código para inserir dados em página existente
+        # acessar página do processo
+        pagina_processo = workbook[numero_processo]
+        # criar nome das colunas
+        pagina_processo['A1'].value = "Número Processo"
+        pagina_processo['B1'].value = "Data Distribuição"
+        pagina_processo['C1'].value = "Movimentações"
+        # adicionar número do processo
+        pagina_processo['A2'].value = numero_processo
+        # adicionar data de distribuição
+        pagina_processo['B2'].value = data_distribuicao
+        # adicionar movimentações
+        for index, linha in enumerate(pagina_processo.iter_rows(min_row=2,max_row=len(lista_movimentacoes),min_col=3,max_col=3)):
+            for celula in linha:
+                celula.value = lista_movimentacoes[index]
+        workbook.save('dados.xlsx')
+        driver.close()
+        sleep(5)
+        driver.switch_to.window(driver.window_handles[0])
+    except Exception as error:
+        # código para criar uma pagina do zero e inserir as informações
+        workbook.create_sheet(numero_processo)
+        # acessar página do processo
+        pagina_processo = workbook[numero_processo]
+        # criar nome das colunas
+        pagina_processo['A1'].value = "Número Processo"
+        pagina_processo['B1'].value = "Data Distribuição"
+        pagina_processo['C1'].value = "Movimentações"
+        # adicionar número do processo
+        pagina_processo['A2'].value = numero_processo
+        # adicionar data de distribuição
+        pagina_processo['B2'].value = data_distribuicao
+        # adicionar movimentações
+        for index, linha in enumerate(pagina_processo.iter_rows(min_row=2,max_row=len(lista_movimentacoes),min_col=3,max_col=3)):
+            for celula in linha:
+                celula.value = lista_movimentacoes[index]
     workbook.save('dados.xlsx')
     driver.close()
     sleep(5)
